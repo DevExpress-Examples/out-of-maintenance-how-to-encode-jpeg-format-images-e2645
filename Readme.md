c870a10771791836973a216783788c91d34522e0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Encode JPEG Format Images
+
+
+<p>This example shows how to save images displayed within the DXGrid control to a data source in JPEG format. To do this, the image editor's <i>Convert</i><i>EditValue</i> event is handled to encode JPEG format images before they are posted to a data source.</p>
+
+<br/>
+
+
---
+++ VB/InplaceImageEdit/MainWindow.xaml.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.IO
+Imports System.Reflection
+Imports System.Windows
+Imports System.Windows.Media.Imaging
+
+Namespace InplaceImageEdit
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			Dim dataSet As New DataSet()
+			Dim [assembly] As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly()
+			dataSet.ReadXmlSchema([assembly].GetManifestResourceStream("NWindCategoriesSchema.xml"))
+			dataSet.ReadXml([assembly].GetManifestResourceStream("NWindCategories.xml"))
+			grid.DataSource = dataSet.Tables(0).DefaultView
+		End Sub
+		Private Sub ImageEditSettings_ConvertEditValue(ByVal sender As DependencyObject, ByVal e As DevExpress.Xpf.Editors.ConvertEditValueEventArgs)
+			Using stream As New MemoryStream()
+				Dim encoder As New JpegBitmapEncoder()
+				encoder.Frames.Add(BitmapFrame.Create(CType(e.ImageSource, BitmapSource)))
+				encoder.Save(stream)
+				e.EditValue = stream.ToArray()
+				e.Handled = True
+			End Using
+		End Sub
+	End Class
+End Namespace
